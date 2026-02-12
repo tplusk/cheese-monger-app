153bae73ae101466ac4136ce01ca9eb7315b780c
tplusk <macfelelhun@gmail.com>
2026-02-12 14:59:05 +0000

    add header, main elements

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,16 +3,20 @@ import cheesesOnTable from "../images/cheesesOnTable.jpg";
 export default function Home() {
   return (
     <div>
-      <div>
-        <h2>Welcome!</h2>
-      </div>
-      <div className="pics">
-        <img
-          id="cheesesOnTable"
-          alt="cheesesOnTable"
-          src={cheesesOnTable}
-        ></img>
-      </div>
+      <header>
+        <div>
+          <h2 id="welcome">Welcome</h2>
+        </div>
+      </header>
+      <main>
+        <div className="pics">
+          <img
+            id="cheesesOnTable"
+            alt="cheesesOnTable"
+            src={cheesesOnTable}
+          ></img>
+        </div>
+      </main>
     </div>
   );
 }
