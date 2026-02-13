225ee60e31cd04ba20978c18d10130704afeabb3
tplusk <macfelelhun@gmail.com>
2026-02-13 10:27:19 +0000

    create data folder and move productList, retailersList

---
+++ src/data/retailersList.js
@@ -0,0 +1,52 @@
+export const retailersList = [
+  {
+    name: "The Cheese Board",
+    address: "2847 North Avenue, Milwaukee, WI 53211",
+    phone: "(414) 555-0298",
+  },
+  {
+    name: "Lakefront Provisions",
+    address: "156 Water Street, Green Bay, WI 54301",
+    phone: "(920) 555-0431",
+  },
+  {
+    name: "Main Street Market",
+    address: "4521 Main Street, Madison, WI 53703",
+    phone: "(608) 555-0762",
+  },
+  {
+    name: "Harbor View Grocery",
+    address: "893 Lakeshore Drive, Sturgeon Bay, WI 54235",
+    phone: "(920) 555-0145",
+  },
+  {
+    name: "Northwoods Natural Foods",
+    address: "2103 Pine Ridge Road, Rhinelander, WI 54501",
+    phone: "(715) 555-0889",
+  },
+  {
+    name: "Capitol Square Deli",
+    address: "67 State Street, Madison, WI 53703",
+    phone: "(608) 555-0234",
+  },
+  {
+    name: "Prairie Home Pantry",
+    address: "1842 Highway 18, Prairie du Chien, WI 53821",
+    phone: "(608) 555-0567",
+  },
+  {
+    name: "River Bend Specialty Foods",
+    address: "3456 Riverside Boulevard, La Crosse, WI 54601",
+    phone: "(608) 555-0923",
+  },
+  {
+    name: "Copper Kettle Market",
+    address: "729 Washington Avenue, Oshkosh, WI 54901",
+    phone: "(920) 555-0612",
+  },
+  {
+    name: "Badger State Provisions",
+    address: "5012 University Avenue, Eau Claire, WI 54701",
+    phone: "(715) 555-0348",
+  },
+];
